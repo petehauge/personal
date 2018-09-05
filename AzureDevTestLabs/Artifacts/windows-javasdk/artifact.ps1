c682a39159675031a2844cb6feffc83c4fe68754
Peter Hauge <phauge@microsoft.com>
2018-09-05 16:40:04 -0400

    Updating Java SDK to enable multiple versions

--- AzureDevTestLabs/Artifacts/windows-javasdk/artifact.ps1
+++ AzureDevTestLabs/Artifacts/windows-javasdk/artifact.ps1
@@ -1,2 +1,27 @@
-# Instal wix with chocolatey installer
-InstallChocoPackages jdk8
+Param
+(
+	[Parameter(Mandatory=$true)]
+    [string] $JDKVersion
+)
+
+Write-Output "Version of JDK to install: $JDKVersion"
+
+if ($JDKVersion -eq "jdk10") {
+    # Instal wix with chocolatey installer
+    Write-Output "Installing via chocolatey package 'jdk10'"
+    InstallChocoPackages jdk10
+}
+else if ($JDKVersion -eq "jdk8") {
+    # Instal wix with chocolatey installer
+    Write-Output "Installing via chocolatey package 'jdk8'"
+    InstallChocoPackages jdk8
+}
+else if ($JDKVersion -eq "jdk7") {
+    Write-Output "TODO!"
+}
+else if ($JDKVersion -eq "jdk6") {
+    # Instal wix with chocolatey installer
+    Write-Output "Installing via chocolatey package 'java.jdk'"
+    InstallChocoPackages java.jdk 
+}
+
