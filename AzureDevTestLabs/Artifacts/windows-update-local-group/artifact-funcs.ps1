dfb0df8dfbf96285b72f9166e4c905c2f61c0b94
Peter Hauge <phauge@microsoft.com>
2019-02-13 20:09:45 -0500

    Initial testing/investigation on windows update group artifact

---
+++ AzureDevTestLabs/Artifacts/windows-update-local-group/artifact-funcs.ps1
@@ -0,0 +1,175 @@
+﻿function Handle-LastError
+{
+    [CmdletBinding()]
+    param(
+    )
+
+    $message = $error[0].Exception.Message
+    if ($message)
+    {
+        Write-Host -Object "ERROR: $message" -ForegroundColor Red
+        Write-Host -Object $error -ForegroundColor Red
+    }
+    
+    # IMPORTANT NOTE: Throwing a terminating error (using $ErrorActionPreference = "Stop") still
+    # returns exit code zero from the PowerShell script when using -File. The workaround is to
+    # NOT use -File when calling this script and leverage the try-catch-finally block and return
+    # a non-zero exit code from the catch block.
+    exit -1
+}
+
+function Download-File ($downloadUrl, $targetFile)
+{
+    Write-Output ("Downloading installation files from URL: $downloadUrl to $targetFile")
+    $targetFolder = Split-Path $targetFile
+
+    if((Test-Path -path $targetFolder) -eq $false)
+    {
+        Write-Output "Creating folder $targetFolder"
+        New-Item -ItemType Directory -Force -Path $targetFolder | Out-Null
+    }
+
+    #Download the file
+    $downloadAttempts = 0
+    do
+    {
+        $downloadAttempts++
+
+        try
+        {
+            [Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11, Tls, Ssl3"
+            $WebClient = New-Object System.Net.WebClient
+            $WebClient.DownloadFile($downloadUrl,$targetFile)
+            break
+        }
+        catch [Exception]
+        {
+            Write-Output "Caught exception during download..."
+            if ($_.Exception.InnerException){
+                $exceptionMessage = $_.InnerException.Message
+                Write-Output "InnerException: $exceptionMessage"
+            }
+            else {
+                $exceptionMessage = $_.Message
+                Write-Output "Exception: $exceptionMessage"
+            }
+        }
+
+    } while ($downloadAttempts -lt 5)
+
+    if($downloadAttempts -eq 5)
+    {
+        Write-Error "Download of $downloadUrl failed repeatedly. Giving up."
+    }
+}
+function InstallChocoPackages ($packageList)
+{
+    $chocoScriptFile = "$PSScriptRoot\ChocolateyPackageInstaller.ps1"
+    if(Test-Path $chocoScriptFile)
+    {
+        Invoke-Expression "$chocoScriptFile -Packages $packageList"
+    }
+    else
+    {
+        throw "Unable to find chocolatey install script at $chocoScriptFile"
+    }
+}
+
+function Invoke-Process
+{
+    [CmdletBinding()]
+    param (
+
+        [string] $FileName = $(throw 'The FileName must be provided'),
+        [string] $Arguments = '',
+        [Array] $ValidExitCodes = @()
+    )
+    
+    Write-Host "Running command '$FileName $Arguments'"
+
+    # Prepare specifics for starting the process that will install the component.
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo -Property @{
+        Arguments = $Arguments
+        CreateNoWindow = $true
+        ErrorDialog = $false
+        FileName = $FileName
+        RedirectStandardError = $true
+        RedirectStandardInput = $true
+        RedirectStandardOutput = $true
+        UseShellExecute = $false
+        Verb = 'runas'
+        WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden
+        WorkingDirectory = $PSScriptRoot
+    }
+
+    # Initialize a new process.
+    $process = New-Object System.Diagnostics.Process
+    try
+    {
+        # Configure the process so we can capture all its output.
+        $process.EnableRaisingEvents = $true
+        # Hook into the standard output and error stream events
+        Register-ObjectEvent -SourceIdentifier OnErrorDataReceived $process "ErrorDataReceived" `
+            `
+            {
+                param
+                (
+                    [System.Object] $sender,
+                    [System.Diagnostics.DataReceivedEventArgs] $e
+                )
+                foreach ($s in $e.Data) { if ($s) { Write-Host $err $s -ForegroundColor Red } }
+            }
+
+        Register-ObjectEvent -SourceIdentifier OnOutputDataReceived $process "OutputDataReceived" `
+            `
+            {
+                param
+                (
+                    [System.Object] $sender,
+                    [System.Diagnostics.DataReceivedEventArgs] $e
+                )
+                foreach ($s in $e.Data) { if ($s -and $s.Trim('. ').Length -gt 0) { Write-Host $s } }
+            }
+
+        $process.StartInfo = $startInfo;
+        # Attempt to start the process.
+        if ($process.Start())
+        {
+            # Read from all redirected streams before waiting to prevent deadlock.
+            $process.BeginErrorReadLine()
+            $process.BeginOutputReadLine()
+            # Wait for the application to exit for no more than 5 minutes.
+            $process.WaitForExit(300000) | Out-Null
+        }
+
+        # Ensure we extract an exit code, if not from the process itself.
+        $exitCode = $process.ExitCode
+        # Determine if process requires a reboot.
+        if ($exitCode -eq 3010)
+        {
+            Write-Host 'The recent changes indicate a reboot is necessary. Please reboot at your earliest convenience.'
+        }
+        elseif ($ValidExitCodes.Contains($exitCode))
+        {
+            Write-Host "$FileName exited with expected valid exit code: $exitCode"
+            # Override to ensure the overall script doesn't fail.
+            $LASTEXITCODE = 0
+        }
+        # Determine if process failed to execute.
+        elseif ($exitCode -gt 0)
+        {
+            # Throwing an exception at this point will stop any subsequent
+            # attempts for deployment.
+            throw "$FileName exited with code: $exitCode"
+        }
+    }
+    finally
+    {
+        # Free all resources associated to the process.
+        $process.Close();
+        # Remove any previous event handlers.
+        Unregister-Event OnErrorDataReceived -Force | Out-Null
+        Unregister-Event OnOutputDataReceived -Force | Out-Null
+    }
+}
+
---
+++ AzureDevTestLabs/Artifacts/windows-update-local-group/artifact.ps1
@@ -0,0 +1,59 @@
+Param
+(
+	[Parameter(Mandatory=$true)]
+    [string] $Users,
+
+    [Paramter(Mandatory=$true)]
+    [string] $Group,
+
+    [Parameter]
+    [bool] $KeepSystemAccounts
+)
+
+Write-Output "---------------------------------"
+Write-Output "windows-update-local-group artifact called with the following parameters:"
+Write-Output "     Users = $Users"
+Write-Output "     Group = $Group"
+Write-Output "     KeepSystemAccounts = $KeepSystemAccounts"
+Write-Output "---------------------------------"
+
+Write-Output "All Existing Groups + Users:"
+$groups = Get-LocalGroup
+foreach ($grp in $groups) {
+    Write-Output "Group Name: $grp"
+    $users = ([ADSI]"WinNT://./$grp").psbase.Invoke('Members') | % {
+                         ([ADSI]$_).InvokeGet('AdsPath')
+                     }
+    $users | ForEach-Object {Write-Output "   $_"}
+}
+Write-Output "---------------------------------"
+
+$systemAccounts = @{
+    "Administrators" = @("Administrator")
+    "Event Log Readers" = @("NT AUTHORITY/NETWORK SERVICE")
+    "Guests" = @("Guest")
+    "IIS_IUSRS" = @("NT Authority/IUSR")
+    "Performance Log Users" = @("NT AUTHORITY/INTERACTIVE")
+    "System Managed Accounts Group" = @("DefaultAccount")
+    "Users" = @("NT AUTHORITY/INTERACTIVE", "NT AUTHORITY/Authenticated Users")
+}
+
+<#
+# Is this a valid group?
+$groupObj = Get-LocalGroup | Where-Object {$_.Name -eq $Group}
+if ($groupObj) {
+    # If we got here, it's a valid group - so let's get the current list of users
+    # We can't use Get-LocalGroupMember because of a powershell bug:  https://github.com/PowerShell/PowerShell/issues/2996 
+    # Method copied from here:  https://p0w3rsh3ll.wordpress.com/2016/06/14/any-documented-adsi-changes-in-powershell-5-0/
+    $existingUsers = ([ADSI]"WinNT://./$Group").psbase.Invoke('Members') | % {
+                         ([ADSI]$_).InvokeGet('AdsPath')
+                     }
+}
+else {
+    Write-Error "Unable to find local group named $Group "
+    Write-Output "Available Local Groups:"
+    Get-LocalGroup | Write-Output
+}
+
+
+#>
